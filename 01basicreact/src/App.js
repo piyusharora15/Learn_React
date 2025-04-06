e88ca3403f82cfdff988f874a8c62e992ccc65cb
Piyush Arora <piyusharora1504@gmail.com>
2025-04-06 11:46:15 +0530

    created my first basic react project

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,7 @@
+function App() {
+  return (
+    <><h1>Hello React! | Learnt reading package.json,creating a react app using two techniques- npm create-reactapp and npm create vite@latest.</h1></>
+  );
+}
+
+export default App;
