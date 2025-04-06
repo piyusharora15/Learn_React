4a7477cc9600d7e31db045f49965e96000ff946a
Piyush Arora <piyusharora1504@gmail.com>
2025-04-06 17:29:39 +0530

    created a new component and rendered it

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,6 +1,16 @@
+import Chai from "./chai";
+// while making a component, the first letter must be upper-case.
+// If we created react-app using vite, then we have to give filename as .jsx, it's convention.
+// In jsx, we can only export one element.
+// In order to return multiple elements, we have several ways: <div></div> enclose the elements in div element.
+// Best way is <>....</> this is called fragment, within this fragment enclose multiple elements, it will be treated as returning a single element.
 function App() {
   return (
-    <h1>Hello React with Vite! | Piyush </h1>
+    <>
+    <Chai />
+    <h1>Chai aur React</h1>
+    <p>Test Para</p>
+    </>
   );
 }
 
