b843a8d2c5358ba2dd79faa27616b2422bcd47f1
Piyush Arora <piyusharora1504@gmail.com>
2025-04-07 12:51:28 +0530

    created new elements and rendered it

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,9 +1,26 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React  from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+// App is just a function so we can write it as a function here and even execute it. But it's not good for optimisation.
+// Behind the scenes, vite uses a transpiler(Babel) that converts HTML into jsx which is understood by react.
+// When we create a component in react by default babel(Transpiler) will inject a element React.createElement()
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+function MyApp(){
+  return (
+    <div>
+      <h1>Custom App | chai</h1>
+    </div>
+  )
+}
+const anotherElement = (
+  <a href="https://google.com" target='_blank'>Visit Google</a>
+)
+
+const reactElement = React.createElement(
+  'a',
+  {href: 'https://google.com', target: '_blank'},
+  'Click me to visit Google'
+)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <App />
 )
